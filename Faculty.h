54d07046e4fe03f1220b37c5a447ef3cd0518d33
Tom Moore <thommoore@chapman.edu>
2024-04-25 16:06:38 -0700

    Inital Commit
    commit

---
+++ Faculty.h
@@ -0,0 +1,13 @@
+#ifndef FACULTY_H
+#define FACULTY_H
+
+#include <iostream>
+
+using namespace std;
+
+class Faculty{
+    public:
+    private:
+};
+
+#endif
---
+++ Student.h
@@ -0,0 +1,13 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+
+using namespace std;
+
+class Student{
+    public:
+    private:
+};
+
+#endif
